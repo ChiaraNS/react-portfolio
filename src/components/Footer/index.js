ed1960f9e81433af1906fe35b297be721fd4fa94
Key <cns3.22202@gmail.com>
2022-02-02 01:45:59 -0600

    footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <nav>
+                <a href='https://github.com/ChiaraNS'>
+                    <img src='' className='icon' alt='Github'></img>
+                </a>
+                <a href='https://www.linkedin.com/in/chiara-schafer-9659501a9/'>
+                    <img src='' className='icon' alt='Linkedin'></img>
+                </a>
+            </nav>
+        </footer>
+    )
+}
+
+export default Footer;
