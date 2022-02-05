a65e87579c418fcd60db4e48df055c57d1b953a6
Key <cns3.22202@gmail.com>
2022-02-04 21:35:05 -0600

    resume

---
+++ src/components/Resume/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function Resume() {
+
+    return (
+        <section>
+            <div className='resume'>
+                <h2>Resume</h2>
+                <a href='' download>PDF</a>
+                <div>
+                    <br></br>
+                    <h4>Experienced In:</h4>
+                        <ul className='experience'>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>JavaScript</li>
+                            <li>Bootstrap</li>
+                            <li>Reactstrap</li>
+                            <li>React</li>
+                            <li>JQuery</li>
+                            <li>RESTful APIs</li>
+                            <li>Express</li>
+                            <li>MySQL</li>
+                            <li>Sequelize</li>
+                            <li>Node</li>
+                            <li>Inquirer</li>
+                            <li>Express</li>
+                            <li>Dotenv</li>
+                            <li>Jest</li>
+                        </ul>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Resume;
